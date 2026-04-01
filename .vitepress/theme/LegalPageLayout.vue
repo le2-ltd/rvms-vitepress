b9c26371a1bc1f4f441fa5798323e7fa4cedf7e1
蘇 <su@le2.ltd>
2026-04-01 17:24:20 +0800

    privacy-policy

---
+++ .vitepress/theme/LegalPageLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="legal-page">
+    <div class="legal-page-shell vp-doc">
+      <Content />
+    </div>
+  </div>
+</template>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,17 +1,25 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
+import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import LegalPageLayout from './LegalPageLayout.vue'
 import './style.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    const { frontmatter } = useData()
+
+    if (frontmatter.value.layout === 'LegalPageLayout') {
+      return h(LegalPageLayout)
+    }
+
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
+  enhanceApp({ app }) {
+    app.component('LegalPageLayout', LegalPageLayout)
   }
 }
